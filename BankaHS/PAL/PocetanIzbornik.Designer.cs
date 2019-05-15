fa88a01900ac51eb3abe16eb164f3ba88fa8715a
sosko123 <hsostaric@foi.hr>
2019-05-15 21:42:14 +0200

    Ureživanje izbornika

--- BankaHS/PAL/PocetanIzbornik.Designer.cs
+++ BankaHS/PAL/PocetanIzbornik.Designer.cs
@@ -29,8 +29,9 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.odjaviSeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.adminStraniceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.odjaviSeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnOtvoriUsluge = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,37 +41,47 @@
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.adminStraniceToolStripMenuItem,
             this.odjaviSeToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(939, 0);
+            this.menuStrip1.Location = new System.Drawing.Point(960, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
             this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.menuStrip1.Size = new System.Drawing.Size(128, 554);
+            this.menuStrip1.Size = new System.Drawing.Size(107, 554);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
-            // 
-            // odjaviSeToolStripMenuItem
-            // 
-            this.odjaviSeToolStripMenuItem.Name = "odjaviSeToolStripMenuItem";
-            this.odjaviSeToolStripMenuItem.Size = new System.Drawing.Size(111, 19);
-            this.odjaviSeToolStripMenuItem.Text = "Odjava";
-            this.odjaviSeToolStripMenuItem.Click += new System.EventHandler(this.odjaviSeToolStripMenuItem_Click);
             // 
             // adminStraniceToolStripMenuItem
             // 
             this.adminStraniceToolStripMenuItem.Name = "adminStraniceToolStripMenuItem";
-            this.adminStraniceToolStripMenuItem.Size = new System.Drawing.Size(111, 19);
+            this.adminStraniceToolStripMenuItem.Size = new System.Drawing.Size(90, 19);
             this.adminStraniceToolStripMenuItem.Text = "Admin stranica";
             this.adminStraniceToolStripMenuItem.Click += new System.EventHandler(this.adminStraniceToolStripMenuItem_Click);
+            // 
+            // odjaviSeToolStripMenuItem
+            // 
+            this.odjaviSeToolStripMenuItem.Name = "odjaviSeToolStripMenuItem";
+            this.odjaviSeToolStripMenuItem.Size = new System.Drawing.Size(90, 19);
+            this.odjaviSeToolStripMenuItem.Text = "Odjava";
+            this.odjaviSeToolStripMenuItem.Click += new System.EventHandler(this.odjaviSeToolStripMenuItem_Click);
+            // 
+            // btnOtvoriUsluge
+            // 
+            this.btnOtvoriUsluge.Location = new System.Drawing.Point(30, 48);
+            this.btnOtvoriUsluge.Name = "btnOtvoriUsluge";
+            this.btnOtvoriUsluge.Size = new System.Drawing.Size(125, 60);
+            this.btnOtvoriUsluge.TabIndex = 1;
+            this.btnOtvoriUsluge.Text = "Otvaranje usluga";
+            this.btnOtvoriUsluge.UseVisualStyleBackColor = true;
             // 
             // PocetanIzbornik
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnOtvoriUsluge);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "PocetanIzbornik";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PocetanIzbornik";
@@ -87,5 +98,6 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem odjaviSeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem adminStraniceToolStripMenuItem;
+        private System.Windows.Forms.Button btnOtvoriUsluge;
     }
 }
